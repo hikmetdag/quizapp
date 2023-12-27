d67bc9cfeb0bf28152fbb65a020211331cfa65f8
hikmetdag <temkihgad@gmail.com>
2023-12-27 23:47:04 +0100

    question

--- client/src/components/question.js
+++ client/src/components/question.js
@@ -1,21 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useFetchQestion } from "../hooks/fetchHook";
 import data from "../database/data";
+import { useSelector } from "react-redux";
 
 export default function Question() {
   const [checked, setChecked] = useState(undefined);
   const [{ isLoading, apiData, serverError }] = useFetchQestion();
   const quest = data[0];
 
+  const questions = useSelector((state) => state.questions?.queue?.[state.questions?.trace]);
+  const trace = useSelector((state) => state.questions?.trace);
+
   useEffect(() => {
-    console.log(apiData);
-  },[apiData] );
+    console.log(trace);
+  });
+
 
   const onSelect = () => {
     setChecked(true);
     console.log("selected");
   };
-
+  if (isLoading) return <h3>isLoading</h3>;
+  if (serverError) return <h3>{serverError || "Unknown Error"}</h3>;
   return (
     <div className="questions">
       <h2>{quest.question}</h2>
--- client/src/redux/question_reducer.js
+++ client/src/redux/question_reducer.js
@@ -3,9 +3,11 @@ import { createSlice } from "@reduxjs/toolkit";
 export const questionReducer = createSlice({
   name: "questions",
   initialState: {
-    queue: [],
-    answers: [],
-    trace: 0,
+    questions: {
+      queue: {},
+      answer:[],
+      trace: 0,
+    }
   },
   reducer: {
     startExamAction: (state, action) => {
