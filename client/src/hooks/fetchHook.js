6c08686276ea0ff17e11f1d23e0953c421a9d3c7
hikmetdag <temkihgad@gmail.com>
2024-01-06 21:07:51 +0100

    contexts

--- client/src/hooks/fetchHook.js
+++ client/src/hooks/fetchHook.js
@@ -1,58 +1,37 @@
-import { useEffect, useState } from "react"
-import { useDispatch } from "react-redux";
-import { getServerData } from "../helper/helper";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-/** redux actions */
-import * as Action from '../redux/question_reducer'
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-/** fetch question hook to fetch api data and set value to store */
-export const useFetchQestion = () => {
-    const dispatch = useDispatch();   
-    const [getData, setGetData] = useState({ isLoading : false, apiData : [], serverError: null});
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        setGetData(prev => ({...prev, isLoading : true}));
-
-        /** async function fetch backend data */
-        (async () => {
-            try {
-                const [{ questions, answers }] = await getServerData(`${process.env.REACT_APP_SERVER_HOSTNAME}/api/questions`, (data) => data)
-                
-                if(questions.length > 0){
-                    setGetData(prev => ({...prev, isLoading : false}));
-                    setGetData(prev => ({...prev, apiData : questions}));
-
-                    /** dispatch an action */
-                    dispatch(Action.startExamAction({ question : questions, answers }))
-
-                } else{
-                    throw new Error("No Question Avalibale");
-                }
-            } catch (error) {
-                setGetData(prev => ({...prev, isLoading : false}));
-                setGetData(prev => ({...prev, serverError : error}));
-            }
-        })();
-    }, [dispatch]);
-
-    return [getData, setGetData];
-}
-
-
-/** MoveAction Dispatch function */
-export const MoveNextQuestion = () => async (dispatch) => {
+  const reFetch = async () => {
+    setLoading(true);
     try {
-        dispatch(Action.moveNextAction()); /** increase trace by 1 */
-    } catch (error) {
-        console.log(error)
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
     }
-}
+    setLoading(false);
+  };
 
-/** PrevAction Dispatch function */
-export const MovePrevQuestion = () => async (dispatch) => {
-    try {
-        dispatch(Action.movePrevAction()); /** decrease trace by 1 */
-    } catch (error) {
-        console.log(error)
-    }
-}
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
